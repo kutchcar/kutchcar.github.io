7a4173ce48e73aec2a9c8d57b0c68b8e1f17b53f
Juned Khatri <juned.khatri31@gmail.com>
2024-05-19 17:26:27 +0530

    Initial commit

---
+++ js/contact.js
@@ -0,0 +1,15 @@
+(function ($) {
+    $('#contact-form').on('submit', (event) => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const message = event.target.message.value;
+        const whatsappMessage = message + '\n\n' + '- ' + name
+        const data = {
+            text: whatsappMessage
+        }
+        const searchParams = new URLSearchParams(data);
+        const number = '918849743405'
+        const whatsappUrl = new URL(`https://wa.me/${number}/?`) + searchParams
+        location.href = whatsappUrl
+    })
+  })(jQuery);
